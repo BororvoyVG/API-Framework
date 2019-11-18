dd839b91ba885d57c5d9afa9e264fea809601115
Borovoy <ilovemetallist@gmail.com>
2019-11-18 08:48:29 +0200

    setup lig4j unsuccessful

---
+++ API_Framework/src/main/java/Log4j2HelloWorldExample.java
@@ -0,0 +1,11 @@
+import lombok.extern.log4j.Log4j2;
+
+
+@Log4j2
+public class Log4j2HelloWorldExample {
+
+    public static void main(String[] args) {
+        log.error("error");
+        log.info("Hello");
+   }
+}
