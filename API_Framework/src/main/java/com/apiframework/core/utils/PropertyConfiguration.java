9c6c77e8732d6193404eb1484dd2ad1f6fd3f952
Borovoy <ilovemetallist@gmail.com>
2019-12-16 00:40:48 +0200

    PropertyConfiguration added, now use prop file PropertyConfiguration.getConfig()

---
+++ API_Framework/src/main/java/com/apiframework/core/utils/PropertyConfiguration.java
@@ -0,0 +1,43 @@
+package com.apiframework.core.utils;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+@Slf4j
+public class PropertyConfiguration {
+
+    public static Configuration getConfig() {
+
+        Parameters params = new Parameters();
+        FileBasedConfigurationBuilder<FileBasedConfiguration> builder =
+                new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+                        .configure(params.properties()
+                                .setFileName("application.properties"));
+
+        Configuration config = null;
+        try {
+            config = builder.getConfiguration();
+        } catch (ConfigurationException cex) {
+            log.error("Couldn't load configuration file: {}", cex.getMessage());
+        }
+        return config;
+    }
+
+    public static String getHomeUrl() {
+        return getConfig().getString("home.url");
+    }
+
+    public static String getUserLogin() {
+        return getConfig().getString("login");
+    }
+
+    public static String getUserPassword() {
+        return getConfig().getString("password");
+    }
+}
