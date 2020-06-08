61b543b1b5df5e8276900a3a5e48fcd00c03b671
BorovoyVG <ilovemetallist@gmail.com>
2020-06-08 15:22:43 +0300

    added @slfj, required methods

--- API_Framework/src/main/java/com/apiframework/pages/LoginPage.java
+++ API_Framework/src/main/java/com/apiframework/pages/LoginPage.java
@@ -1,17 +1,20 @@
 package com.apiframework.pages;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 
 @Getter
+@Slf4j
 public class LoginPage extends AbstractPage {
 
     private static final String SIGN_IN_LINK = "//span[@id='gh-ug']//a[contains(@href,'signin.ebay.com')]";
-    private static final String EMAIL_INPUT_FIELD = "//span[@class='textbox']//input[@id='userid']";
+    private static final String EMAIL_INPUT_FIELD = "//div[@class='textbox']//input[@id='userid']";
     private static final String PASS_INPUT_FIELD = "//input[@id='pass']";
     private static final String SIGN_IN_BUTTON = "//button[@id='sgnBt']";
+    private static final String SIGN_IN_CONTINUE_BUTTON = "//button[@id='signin-continue-btn']";
     private static final String PROFILE_BUTTON = "//button[@class='gh-ua gh-control' and (contains(.,'Hi'))]";
 
     private void clickOnSingInLink() {
@@ -23,16 +26,21 @@ public class LoginPage extends AbstractPage {
     }
 
     private void setPassword(final String password) {
-        $x(PASS_INPUT_FIELD).sendKeys(password);
+        $x(PASS_INPUT_FIELD).shouldBe(visible).sendKeys(password);
     }
 
     private void clickOnSignInButton() {
-        $x(SIGN_IN_BUTTON).click();
+        $x(SIGN_IN_BUTTON).shouldBe(visible).click();
+    }
+
+    private void clickOnContinueButton() {
+        $x(SIGN_IN_CONTINUE_BUTTON).shouldBe(visible).click();
     }
 
     public void logInToEbay(final String email, final String password) {
         clickOnSingInLink();
         setEmail(email);
+        clickOnContinueButton();
         setPassword(password);
         clickOnSignInButton();
     }
